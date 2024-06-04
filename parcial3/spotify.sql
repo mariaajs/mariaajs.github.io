4d5318cb99230956140a1ecfa8e192790edbdc94
mariaajs <159510913+mariaajs@users.noreply.github.com>
2024-06-03 18:52:45 -0700

    Practica #7 03/06/24

--- parcial3/spotify.sql
+++ parcial3/spotify.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-05-2024 a las 03:03:29
+-- Tiempo de generación: 04-06-2024 a las 03:52:02
 -- Versión del servidor: 10.1.39-MariaDB
 -- Versión de PHP: 7.3.5
 
@@ -34,6 +34,35 @@ CREATE TABLE `artistas` (
   `descripcion` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Volcado de datos para la tabla `artistas`
+--
+
+INSERT INTO `artistas` (`id`, `nombre`, `descripcion`) VALUES
+(1, 'Taylor Swift', 'Taylor Alison Swift es una cantante, compositora, productora discográfica, directora, actriz y empresaria estadounidense. Criada en Wyomissing, se mudó a Nashville a los 14 años para realizar una carrera de música country.'),
+(2, 'Justin Bieber', 'Justin Drew Bieber es un cantautor canadiense.'),
+(3, 'Kendrick Lamar', 'Kendrick Lamar Duckworth es un rapero, compositor, productor discográfico estadounidense.'),
+(4, 'Tyler,The creator', 'Tyler Gregory Okonma, mejor conocido artísticamente como Tyler, the Creator, es un rapero, productor discográfico, director, actor y diseñador de moda estadounidense.'),
+(5, 'Lana del Rey', 'Elizabeth Woolridge Grant, ​conocida artísticamente como Lana Del Rey, es una cantante, compositora, modelo, actriz, escritora, productora y poetisa estadounidense. Su música se distingue por su calidad cinematográfica, su estilo retro y su exploración del romance trágico, el glamur y la melancolía.'),
+(6, 'Billie Eilish', 'Billie Eilish Pirate Baird O\'Connell, conocida simplemente como Billie Eilish, es una cantante estadounidense. Obtuvo atención pública por primera vez en diciembre de 2015 con su sencillo debut «Ocean Eyes», escrito y producido por su hermano Finneas O\'Connell, con quien colabora en música y espectá'),
+(7, 'Travis Scott', 'Jacques Berman Webster II, ​ más conocido por su nombre artístico Travis Scott, es un rapero, compositor y productor musical estadounidense.'),
+(8, 'Kanye West', 'Kanye West es un rapero, productor discográfico, diseñador de moda y empresario estadounidense'),
+(9, 'Ariana Grande', 'Ariana Grande-Butera ​ es una cantante, actriz y empresaria estadounidense.​ Comenzó su carrera musical en Broadway a los 15 años en el musical 13'),
+(10, 'Natalia Lafourcade', 'María Natalia Lafourcade Silva, conocida como Natalia Lafourcade, es una cantante, ​ compositora, ​ actriz, ​ productora musical, ​ arreglista, ​ diseñadora​ activista y multinstrumentista mexicana.​ '),
+(11, 'Carla Morrison', 'Carla Patricia Morrison Flores, conocida como Carla Morrison, es una cantante, ​ compositora, ​ actriz, ​ activista​ y productora​ mexicana.');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `artistas_canciones`
+--
+
+CREATE TABLE `artistas_canciones` (
+  `id` int(11) NOT NULL,
+  `artista_id` int(11) NOT NULL,
+  `cancion_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- --------------------------------------------------------
 
 --
@@ -53,6 +82,18 @@ CREATE TABLE `canciones` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `favoritas`
+--
+
+CREATE TABLE `favoritas` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `cancion_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `generos`
 --
 
@@ -60,6 +101,20 @@ CREATE TABLE `generos` (
   `id` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `generos`
+--
+
+INSERT INTO `generos` (`id`, `nombre`) VALUES
+(1, 'kpop'),
+(2, 'jazz'),
+(3, 'pop'),
+(4, 'Musica clasica'),
+(5, 'Blues'),
+(6, 'salsa'),
+(7, 'rock and roll'),
+(8, 'flamenco');
 
 -- --------------------------------------------------------
 
@@ -72,6 +127,15 @@ CREATE TABLE `membresias` (
   `desc` varchar(100) NOT NULL,
   `precio` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `membresias`
+--
+
+INSERT INTO `membresias` (`id`, `desc`, `precio`) VALUES
+(1, 'plan Premium Individual, 1 cuenta para una persona.', 169),
+(2, ' plan Premium Duo, 2 cuentas para una pareja', 129),
+(3, ' plan Premium Familiar ', 199);
 
 -- --------------------------------------------------------
 
@@ -92,6 +156,19 @@ CREATE TABLE `playlist` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `playlist_canciones`
+--
+
+CREATE TABLE `playlist_canciones` (
+  `id` int(11) NOT NULL,
+  `playlist_id` int(11) NOT NULL,
+  `cancion_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
@@ -101,6 +178,13 @@ CREATE TABLE `usuarios` (
   `correo` varchar(150) NOT NULL,
   `membresia_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `correo`, `membresia_id`) VALUES
+(0, 'Ricardo Inzunza', ' @ricardounicornio88', 0);
 
 --
 -- Índices para tablas volcadas
@@ -113,11 +197,27 @@ ALTER TABLE `artistas`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `artistas_canciones`
+--
+ALTER TABLE `artistas_canciones`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `artista_id` (`artista_id`),
+  ADD KEY `cancion_id` (`cancion_id`);
+
+--
 -- Indices de la tabla `canciones`
 --
 ALTER TABLE `canciones`
   ADD PRIMARY KEY (`id`),
   ADD KEY `genero_id` (`genero_id`);
+
+--
+-- Indices de la tabla `favoritas`
+--
+ALTER TABLE `favoritas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `cancion_id` (`cancion_id`);
 
 --
 -- Indices de la tabla `generos`
@@ -139,11 +239,48 @@ ALTER TABLE `playlist`
   ADD KEY `usuario_id` (`usuario_id`);
 
 --
+-- Indices de la tabla `playlist_canciones`
+--
+ALTER TABLE `playlist_canciones`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `playlist_id` (`playlist_id`),
+  ADD KEY `cancion_id` (`cancion_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`),
   ADD KEY `membresia_id` (`membresia_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `artistas`
+--
+ALTER TABLE `artistas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `artistas_canciones`
+--
+ALTER TABLE `artistas_canciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `favoritas`
+--
+ALTER TABLE `favoritas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `playlist_canciones`
+--
+ALTER TABLE `playlist_canciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
